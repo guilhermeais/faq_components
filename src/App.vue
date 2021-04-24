d80b2df4bdca0f6acb1c32509a9d89d6b1ab776d
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2021-04-24 06:51:13 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>
-
+  <div class="app">
+    <div class="container">
+      <transition>
+        <component :is="'faqCategories'" />
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import faqCategories from './components/FaqCategories.vue'
+export default {
+  name: 'App',
+  components: {
+    faqCategories
+  }
+}
 </script>
+
+<style>
+.app {
+  display: grid;
+  grid-template-columns: 305px;
+  grid-template-rows: 29rem;
+  justify-content: center;
+  align-content: center;
+  height: 100vh;
+  background-color: #f5f6f8;
+}
+
+.container {
+  padding: 2rem 0.375rem;
+  padding-bottom: 0.875rem;
+  background-image: linear-gradient(to bottom, #3f4452, #26282c);
+  border-radius: 15px;
+  -webkit-box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+  box-shadow: 0px 8px 21px 3px rgba(0, 0, 0, 0.19);
+  overflow: hidden;
+  position: relative;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,28 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 import { faqCategories } from '@/utils/db.json'
 
 export default createStore({
   state: {
     faqCategories: [],
     questionsCategory: {},
-    answer:{}
+    answer: {}
   },
-  mutations: {},
-  actions: {
-    fetchFaqCategories({commit}){
-      const data = faqCategories
+  mutations: {
+    SET_FAQ_CATEGORIES(state, data) {
+      // pegamos o estado faqCategories e depois recebemos um payload, que no caso são as categorias passadas pelo fetchFaqCategories
+      state.faqCategories = data
     }
   },
-});
+  actions: {
+    fetchFaqCategories({ commit }) {
+      const data = faqCategories
+
+      commit('SET_FAQ_CATEGORIES', data)
+    }
+  },
+  getters: {
+    $faqCategories(state) {
+      return state.faqCategories
+    }
+  }
+})
